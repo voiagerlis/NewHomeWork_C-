8b1bf562b87d3e081da90198f89e8947257f7d50
Vladimir <voiagerlis@yanex.ru>
2023-04-20 21:29:06 +0300

    Ршение задачи № 6

---
+++ Lesson1/Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка является ли введенное пользователем число четным
+
+Console.WriteLine("Пожалуйста, введите число");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double result = num1 % 2;
+if (result == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else
+    Console.WriteLine("Введенное число не является четным");
