8660f02cdead858dbf053c514497ac0b4653570d
Vladimir <voiagerlis@yanex.ru>
2023-04-20 20:15:27 +0300

    Решение задачи № 2

---
+++ Lesson1/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Сравнение двух чисел
+
+Console.WriteLine("Пожалуйста, введите два числа");
+
+int namber1 = Convert.ToInt32(Console.ReadLine());
+int namber2 = Convert.ToInt32(Console.ReadLine());
+int result = (namber1 + namber2 + Math.Abs(namber1 - namber2))/2;
+
+Console.WriteLine("Максимальное число = "+result);
