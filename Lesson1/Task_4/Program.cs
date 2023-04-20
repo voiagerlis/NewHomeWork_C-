18b2a8c1b6865d139b7c136ae622a14c03c1a6d6
Vladimir <voiagerlis@yanex.ru>
2023-04-20 20:46:24 +0300

    Решение задачи № 4

---
+++ Lesson1/Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определение максимального числа из трех введенных ользователем
+
+Console.WriteLine("Пожалуйста, введите три числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) 
+{ 
+    max = num2;
+}
+if (num3 > max )
+{
+    max = num3;
+}
+
+Console.WriteLine("Максимальное число = " + max);
