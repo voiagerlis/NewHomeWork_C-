6307351c333a415b31fb7e8b607b4c34c98773e2
Vladimir <voiager@rambler.ru>
2023-04-21 09:27:16 +0300

    Решение задачи № 8

---
+++ Lesson1/Task_8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показываю четные числа диапазона пользователя
+
+Console.WriteLine("Пожалуйста, введите число");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double result = num1 % 2;
+int index = 2;
+if (result == 0)
+{
+   while (index <= num1)
+   {
+           Console.WriteLine(index);
+           index = index + 2;
+   }
+}
+if (result == 1)
+{
+    while (index <= num1-1)
+    {
+           Console.WriteLine(index);
+           index = index + 2;
+    }
+}
+    //Console.WriteLine("Введенное число не является четным");
