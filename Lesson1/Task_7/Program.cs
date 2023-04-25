0383ea17d117d52db269e0a4d3217d565284c39a
Vladimir <voiager@rambler.ru>
2023-04-25 11:06:35 +0300

    Решение задачи № 7

---
+++ Lesson1/Task_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести цифру второго разряда вводимого числа
+
+Console.WriteLine("Пожалуйста, введите как минимум двузначное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10) {Console.WriteLine("Введенное число меньше десяти, задача не имеет решения");}
+else
+{
+   while (num > 99)  {num = num /10; }
+
+   if (num <= 99)
+   {       num = num % 10;
+        Console.WriteLine($"Число во втором разряде = {num} ");
+   }          
+} 
