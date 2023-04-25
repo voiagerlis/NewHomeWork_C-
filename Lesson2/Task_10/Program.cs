e60754b96e76232608dfa1448b4eaa7a5fb25322
Vladimir <voiager@rambler.ru>
2023-04-25 15:28:36 +0300

    Решение задачи № 10

---
+++ Lesson2/Task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести вторую цифру трехзначного вводимого числа
+
+Console.WriteLine("Пожалуйста, введите трехзначное число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num >999) {Console.WriteLine("Введенное число не является трехзначным");}
+
+else { 
+       num = (num / 10) % 10; 
+       Console.WriteLine($"Число во втором разряде = {num} ");
+     } 
