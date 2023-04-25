4911f27ccb894a1250261fb74a8fa1aee0390d47
Vladimir <voiager@rambler.ru>
2023-04-25 16:17:24 +0300

    Решение задачи № 13

---
+++ Lesson2/Task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести третью цифру трехзначного вводимого числа
+
+Console.WriteLine("Пожалуйста, введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 ) {Console.WriteLine("Третьей цифры нет");}
+
+else { 
+       while (num >1000) {num = num /10; }
+        num = num % 10;
+        Console.WriteLine($"Число во третьем разряде = {num} ");
+      } 
