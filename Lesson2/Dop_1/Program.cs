f24a6ae099957ddba30f3d2f5ac7c93887ecdcd6
Vladimir <voiager@rambler.ru>
2023-04-25 18:08:02 +0300

    Начало решение дополнительной задачи № 1

---
+++ Lesson2/Dop_1/Program.cs
@@ -0,0 +1,33 @@
+﻿//На входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+Console.WriteLine("Пожалуйста, введите число");
+
+double num = Convert.ToDouble(Console.ReadLine());
+double memori_num = num;
+int index = 1; 
+int one_num = 0 ;
+double two_num = 0 ;
+
+if (num < 10) {Console.WriteLine("Введенное число меньше десяти, задача не имеет решения");}
+else
+{
+   while ( num > 10 ) 
+    {
+        num = num /10;
+        index = index * 10;
+    }
+
+   if (num <= 10)
+   {   
+        one_num = (int)num;
+              
+        two_num = memori_num / (index/10);
+      Console.WriteLine(two_num);
+        two_num = two_num % 100;
+      //  two_num = two_num - one_num;
+        //two_num = two_num * index;
+        
+        //Console.WriteLine($"{one_num}{two_num}");
+       // Console.WriteLine(one_num);
+        
+   }          
+} 
