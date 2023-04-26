3de2e048e8380700226f5ac6e5d8dad3db6264fa
Vladimir <voiager@rambler.ru>
2023-04-26 14:39:12 +0300

    Решение второй дополнительной задачи

--- Lesson2/Dop_1/Program.cs
+++ Lesson2/Dop_1/Program.cs
@@ -2,32 +2,50 @@
 Console.WriteLine("Пожалуйста, введите число");
 
 double num = Convert.ToDouble(Console.ReadLine());
-double memori_num = num;
-int index = 1; 
-int one_num = 0 ;
+int index = 1;
 double two_num = 0 ;
-
-if (num < 10) {Console.WriteLine("Введенное число меньше десяти, задача не имеет решения");}
-else
-{
-   while ( num > 10 ) 
+while ( num > 99 ) 
     {
         num = num /10;
         index = index * 10;
     }
+two_num = num % index/10;
 
-   if (num <= 10)
-   {   
-        one_num = (int)num;
-              
-        two_num = memori_num / (index/10);
-      Console.WriteLine(two_num);
-        two_num = two_num % 100;
-      //  two_num = two_num - one_num;
-        //two_num = two_num * index;
+Console.WriteLine(num);
+Console.WriteLine(two_num);
+Console.WriteLine(index);
+// 
+// double memori_num = num;
+// int index = 1; 
+// double one_num = 0 ;
+// double two_num = 0 ;
+// double tmp =0;
+// if (num < 10) {Console.WriteLine("Введенное число меньше десяти, задача не имеет решения");}
+// else
+// {
+//    while ( num > 99 ) 
+//     {
+//         num = num /10;
+//         index = index * 10;
+//     }
+
+//    if (num > 10 && num < 99) { one_num = (int) num / 10; }
+//     Console.WriteLine(one_num);
+
+//    if (num <= 10)
+//    {   
+//         one_num = (int)num;
+//        // Console.WriteLine(one_num);      
+//         two_num = memori_num / (index/10);
+//         Console.WriteLine(two_num);
+//         two_num = (((two_num - one_num * 10)*10 )% 10)*index;
+//        // two_num = (int)(two_num * index); 
+//         Console.WriteLine(two_num);
+//        // Console.WriteLine(two_num);
+      
         
-        //Console.WriteLine($"{one_num}{two_num}");
-       // Console.WriteLine(one_num);
+//         //Console.WriteLine($"{one_num}{two_num}");
+//        // Console.WriteLine(one_num);
         
-   }          
-} 
+//    }          
+//} 
