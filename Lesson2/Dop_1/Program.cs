fa19c4bd958f4f00ac755274c49d11fcad96024b
Vladimir <voiager@rambler.ru>
2023-04-26 17:31:32 +0300

    Пытаюсь поймать баги

--- Lesson2/Dop_1/Program.cs
+++ Lesson2/Dop_1/Program.cs
@@ -6,12 +6,34 @@ double memory_num = num;
 int one_num = 0;
 double two_num = 0;
 int index = 1;
+int count = 1;
+if (num % 1 != 0) 
+{
+    while (num % 1 !=0 )
+    {
+        num = num * 10;
+        count ++ ;
+    }
+}
+//Console.WriteLine($"Count = {count}");
+num = memory_num;
+if (num >=10 && num <= 99)
+{
+
+
+
+    
+}
 if (num < 10) {Console.WriteLine("Введенное число менее 10, задача не имеет решения"); return; }
-while (memory_num > 99)
+
+while (memory_num > 99) 
 {
     memory_num = memory_num / 10;
     index = index * 10;    
 } 
+Console.WriteLine($"Index ={index}");
 two_num = num % index;
+Console.WriteLine($"Второе число ={two_num}");
 one_num = (int)(num / index /10);
-Console.WriteLine($"{one_num}{two_num}");
+Console.WriteLine($"Первое число ={one_num}");
+Console.WriteLine($"{one_num+two_num}");
