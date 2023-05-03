c97c60fc0d2f615570bff71126b934ac27e92901
Vladimir <voiager@rambler.ru>
2023-05-03 19:35:08 +0300

    Решил обязательную и не обязательную задачу № 19

---
+++ Lesson3/Dop19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход любое целое число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Пожалуйста, введите любое целое число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int pallindrom = 0;
+int SizeNum = 0;
+
+int CountLong (int size_number) // метод подсчитывает разрядность введенного числа
+{
+    int count = 1;
+    while (size_number > 10)
+    {
+        size_number = size_number / 10;
+        count = count * 10;
+    }
+
+return count;
+}
+
+int Shifter( int number, int ligth_num) // метод переворачивает число в обратном порядке
+{
+int index = 1;
+int one = 0;
+int tmp_one = 0;
+
+     while (index <= ligth_num)
+    {
+        tmp_one = (num / index) % 10; // получаю очередной разряд числа от меньшего к большему справо на лево
+        one = one * 10 + tmp_one;
+        index = index * 10;    
+    }
+return (one);
+}     
+     SizeNum = CountLong (num); 
+     pallindrom = Shifter(num, SizeNum);
+     if ( num == pallindrom ) { Console.WriteLine($"число {pallindrom}, палиндром числа {num} "); }
+     else { Console.WriteLine($"число {pallindrom}, не является палиндром числа {num} "); }    
+     
+ 
