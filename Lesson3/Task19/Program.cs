5e1b545615788d48bd312e8e4e4f23c588e843a6
Vladimir <voiager@rambler.ru>
2023-05-11 14:51:13 +0300

    Отсылаю на проверку

--- Lesson3/Task19/Program.cs
+++ Lesson3/Task19/Program.cs
@@ -8,19 +8,25 @@ int index = 1;
 int one = 0;
 int tmp_one = 0;
 
-if (num < 9999 || num >99999) {Console.WriteLine("Введенное число не является пятизначным");}
-
-else 
-{ 
-       
-     while (index <= 10000)
+if (num < 9999 || num > 99999)
+{
+    Console.WriteLine("Введенное число не является пятизначным");
+}
+else
+{
+    while (index <= 10000)
     {
         tmp_one = (num / index) % 10; // получаю очередной разряд числа от меньшего к большему справо на лево
         one = one * 10 + tmp_one;
-        index = index * 10;    
+        index = index * 10;
     }
-     
-     if ( num == one ) { Console.WriteLine($"ДА, число  = {one}, палиндром числа {num} "); }
-     else { Console.WriteLine($"НЕТ, число  = {one}, не является палиндром числа {num} "); }    
-     
-} 
+
+    if (num == one)
+    {
+        Console.WriteLine($"ДА, число  = {one}, палиндром числа {num} ");
+    }
+    else
+    {
+        Console.WriteLine($"НЕТ, число  = {one}, не является палиндром числа {num} ");
+    }
+}
