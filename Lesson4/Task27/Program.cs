7db916691c211166c1ed107d4eeda4c0a6c2ef81
Vladimir <voiager@rambler.ru>
2023-05-20 10:46:55 +0300

    Решение задачи № 27

---
+++ Lesson4/Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+//********************************************************************************
+
+Console.WriteLine("Введите целочисленное число отличное от нуля");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 0)
+/// Проверяю условия нуля
+{
+    Console.WriteLine("Вы ввели нуль");
+    return;
+}
+
+/// Если число отрицательное привожу его к модулю
+num = Math.Abs(num);
+
+/// Функция подсчета сумм цифр в числе
+int LeghtNum(int repNum)
+{
+    int tmpNum = 0;
+    while (repNum > 10)
+    {
+        tmpNum = tmpNum + repNum % 10;
+        repNum = repNum / 10;
+    }
+    tmpNum = tmpNum + repNum;
+    return tmpNum;
+}
+
+int result = LeghtNum(num);
+Console.WriteLine($"Сумма цифр в числе = {result}");
