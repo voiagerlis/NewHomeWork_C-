06b831b5a2752cdb1e0879cad218aebba1795e80
Vladimir <voiager@rambler.ru>
2023-05-20 11:34:06 +0300

    Решил переделать через масив

--- Lesson4/Task27/Program.cs
+++ Lesson4/Task27/Program.cs
@@ -6,29 +6,34 @@
 
 Console.WriteLine("Введите целочисленное число отличное от нуля");
 int num = Convert.ToInt32(Console.ReadLine());
-
-if (num == 0)
-/// Проверяю условия нуля
-{
-    Console.WriteLine("Вы ввели нуль");
-    return;
-}
+int [] numArrey; //= new int [];
 
 /// Если число отрицательное привожу его к модулю
 num = Math.Abs(num);
 
-/// Функция подсчета сумм цифр в числе
+/// Функция заполнения массива цифр из числа
 int LeghtNum(int repNum)
 {
     int tmpNum = 0;
+    int index = 0;
     while (repNum > 10)
     {
-        tmpNum = tmpNum + repNum % 10;
+        numArrey [index] = repNum % 10;
         repNum = repNum / 10;
+     index++;
     }
-    tmpNum = tmpNum + repNum;
-    return tmpNum;
+    index++;
+    numArrey [index] = repNum; 
+    
+    //    Console.WriteLine($"tmp = {tmpNum}");
+    // Console.WriteLine($"rep = {repNum}");
+   
+    //tmpNum = tmpNum + repNum;
+    //return tmpNum;
 }
+int SumNumber (int [] arrayN)
+{
 
-int result = LeghtNum(num);
-Console.WriteLine($"Сумма цифр в числе = {result}");
+}
+// int result = LeghtNum(num);
+// Console.WriteLine($"Сумма цифр в числе = {result}");
