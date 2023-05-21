47cea97f556b69bf68f7b920b75a7765c0c8333f
Vladimir <voiagerlis@yanex.ru>
2023-05-21 19:02:13 +0300

    Зешение задачи № 29

---
+++ Lesson4/Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+int[] arrayNum = new int[8];
+Console.WriteLine("Заполните целочисленный массив из восьми злементов");
+int num = 0;
+string str = "";
+for (int i = 0; i < 8; i++)
+{
+    num = Convert.ToInt32(Console.ReadLine());
+    arrayNum[i] = num;
+}
+for (int i = 0; i < 8; i++)
+{
+    str = str + ","+ arrayNum[i];
+       //Console.WriteLine($"Сумма цифр в числе = {arrayNum[i]}");
+}
+Console.WriteLine($"Ваш массив {str}");
