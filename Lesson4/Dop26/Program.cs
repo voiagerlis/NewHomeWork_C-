6ca27d0f2fe941b0962c4e2f157e178d6280926c
Vladimir <voiagerlis@yanex.ru>
2023-05-22 22:18:00 +0300

    почти решил

--- Lesson4/Dop26/Program.cs
+++ Lesson4/Dop26/Program.cs
@@ -4,81 +4,45 @@
 // 89,126 -> 5
 // 0,001->4
 
-/// Метод подсчета чисел при значении от 0 до 1 
-int SummLessOne (double number)
+// Метод подсчета количества цифр числа
+
+int LeghtNum(decimal repNum)
 {
-    while (number <= 1)
+    int res = 0;
+    int count = 0;
+    int counter = 0;
+    decimal integerNum = repNum;
+    integerNum = integerNum - (integerNum % 1);
+    int intNum = Convert.ToInt32(integerNum);
+    while (intNum > 10)
     {
-        num = num * 10;
+        intNum = intNum / 10;
         count++;
     }
-}
-return count;
-
-//// Метод подсчета цифр целочисленног числа
-/// Функция подсчета сумм цифр в числе
-int LeghtNum(int repNum)
-{
-    int tmpNum = 0;
-    while (repNum > 10)
+    count++;
+    if (repNum % 1 != 0)
     {
-        tmpNum = tmpNum + repNum % 10;
-        repNum = repNum / 10;
+        decimal doubleNum = repNum;
+        doubleNum = doubleNum % 1 - doubleNum ;
+        Console.WriteLine($" Дробное число = {doubleNum}");
+        while (doubleNum < 1)
+        {
+            doubleNum = doubleNum * 10;
+            counter++;
+            Console.WriteLine($" Дробное число = {doubleNum}");
+        }
     }
-    tmpNum = tmpNum + repNum;
-    return tmpNum;
+    res = count + counter;
+    return res;
 }
-
 
 
 Console.WriteLine("Введите число");
-double num = Convert.ToDouble(Console.ReadLine());
+decimal num = Convert.ToDecimal(Console.ReadLine());
+int resalt = 0;
 // привожу к модулю
 num = Math.Abs(num);
-int count = 0;
 
-if (num < 1) 
-SummLessOne(num);
-else
-if (num == 10)
-{
-    Console.WriteLine("Сумма цифр в числе = 1");
-    return;
-}
+resalt = LeghtNum(num);
 
-
-
-Console.WriteLine($"Сумма цифр в числе = {count}");
-// else
-// {
-// int digitCount = Convert.ToInt32(Math.Log10(num) + 1);
-// Console.WriteLine("Разрядность числа " + num + " равна " + digitCount);
-// }
-
-
-
-// Console.WriteLine("Введите целочисленное число");
-// double num = Convert.ToDouble(Console.ReadLine());
-
-// /// Если число отрицательное привожу его к модулю
-// num = Math.Abs(num);
-// // if (num == 10)
-// // {
-// //     Console.WriteLine("Сумма цифр в числе = 1");
-// //     return;
-// // }
-// /// Функция подсчета сумм цифр в числе
-// int LeghtNum(int repNum)
-// {
-//     int tmpNum = 0;
-//     while (repNum > 10)
-//     {
-//         tmpNum = tmpNum + repNum % 10;
-//         repNum = repNum / 10;
-//     }
-//     tmpNum = tmpNum + repNum;
-//     return tmpNum;
-// }
-
-// int result = LeghtNum(num);
-// Console.WriteLine($"Сумма цифр в числе = {result}");
+Console.WriteLine($"Количество цифр в числе = {resalt}");
