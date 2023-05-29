ec6659304d0c977fa6db6be453869a99e2c4ae02
Vladimir <voiager@rambler.ru>
2023-05-29 16:04:23 +0300

    Решение задачи Него Фибоначи

--- Lesson6/NegaFibo/Program.cs
+++ Lesson6/NegaFibo/Program.cs
@@ -1,9 +1,15 @@
 ﻿// Задача НЕГАФИБОНАЧЧИ. Задайте число. Составьте массив чисел НегаФибоначчи, в том числе для отрицательных индексов.
 // Пример:
-// для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+// для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1,
+//                                       0-21  13  -8  5  -3  2  -1  1
+//                                          0, 1, 1, 2, 3, 5, 8, 13, 21]
+//                                          0 1  1   2 3  5  8  13  21
+//                                       0-21 13 -8 5 -3 2 -1 1
 // F−1 = 1,
 // F−2 = -1,
 // Fn = F(n+2)−F(n+1).
+
+
 int[] Fibonachi(int num)
 {
     int[] array = new int[num];
@@ -15,17 +21,18 @@ int[] Fibonachi(int num)
     }
     return array;
 }
-int[] NegFibonachi(int[] array)
+int[] NegFibonachi(int[] array, int num)
 {
- array[0] = 1;
-    array[1] = -1;
-    for (int i = 2; i < array.Length; i =i + 2)
+    num = num - 1;
+    int[] arrayNeg = new int[num];
+    arrayNeg[num - 1] = 1;
+    arrayNeg[num - 2] = -1;
+    for (int k = num - 3; k >= 0; k--)
     {
-        array[i] = array[i] * -1;
+        arrayNeg[k] = arrayNeg[k + 2] - arrayNeg[k + 1];
     }
-    return array;
+    return arrayNeg;
 }
-
 
 void PrintArray(int[] array)
 {
@@ -33,11 +40,12 @@ void PrintArray(int[] array)
         Console.Write($"{item} ");
 }
 
+Console.Clear();
 System.Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int[] fibo = new int[num];
 int[] netfibo = new int[num];
 fibo = Fibonachi(num);
-netfibo = NegFibonachi(fibo);
-PrintArray(fibo);
+netfibo = NegFibonachi(fibo, num);
 PrintArray(netfibo);
+PrintArray(fibo);
