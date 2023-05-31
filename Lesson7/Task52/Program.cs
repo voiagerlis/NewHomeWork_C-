4a4f39ac5aad961827ca72725f00bab72d0ff774
Vladimir <voiager@rambler.ru>
2023-05-31 14:24:14 +0300

    Решение задачи № 52

--- Lesson7/Task52/Program.cs
+++ Lesson7/Task52/Program.cs
@@ -1,10 +1,10 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве,
-// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// 17 -> такого числа в массиве нет
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
 
 void FillArray(int[,] array)
 {
@@ -12,7 +12,6 @@ void FillArray(int[,] array)
         for (int j = 0; j < array.GetLength(1); j++)
             array[i, j] = new Random().Next(0, 9);
 }
-
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -22,40 +21,43 @@ void PrintArray(int[,] array)
         System.Console.WriteLine();
     }
 }
-int[] ChekNum(int[,] array, int numer)
+void PrA(double[] array)
 {
-    int[] helper = new int[3];
-    helper[2] = 0;
+    for (int i = 0; i < array.Length; i++)
+
+        System.Console.Write($"{array[i], 3}     ");
+    System.Console.WriteLine();
+}
+
+double[] ArMean(int[,] array, int num)
+{
+    double[] helper = new double[num];
+    double sum = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
+        sum = 0;
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (numer == array[i, j])
-            {
-                helper[0] = i;
-                helper[1] = j;
-                helper[2] = 1;
-            }
+            sum = sum + array[j, i];
         }
+        sum = sum / array.GetLength(1);
+        helper[i] = Math.Round(sum,2);
     }
     return helper;
 }
+
 Console.Clear();
 Console.WriteLine("Введите количество строк");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
 int cols = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите искомое число");
-int num = Convert.ToInt32(Console.ReadLine());
+int num = cols;
 
 int[,] array = new int[rows, cols];
-int[] arNum = new int[2];
+double[] arNum = new double[num];
 FillArray(array);
 PrintArray(array);
-arNum = ChekNum(array, num);
-if (arNum[2] == 1)
-    Console.WriteLine(
-        $"Позиция введенного числа в данном массиве строка {arNum[0]} столбец {arNum[1]}"
-    );
-else
-    Console.WriteLine("Такого числа в массиве нет");
+arNum = ArMean(array, num);
+Console.WriteLine("**********************************");
+PrA(arNum);
