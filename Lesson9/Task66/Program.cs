9609d397f85be006544ce9d798e560e1a856f6cb
Vladimir <voiager@rambler.ru>
2023-06-08 15:50:03 +0300

    Решение задачи № 66

---
+++ Lesson9/Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumNature(int np, int iter, int sum)
+{
+    if (iter == 0)
+        return sum;
+    sum = sum + np++;
+    iter--;
+   return SumNature(np, iter, sum);
+}
+Console.Clear();
+System.Console.WriteLine("Введите нижнюю границу диапазона");
+int numBotton = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите верхнюю границу диапазона");
+int numTop = Convert.ToInt32(Console.ReadLine());
+int sum = numTop;
+if (numBotton < numTop)
+{
+    int iter = (numTop - numBotton);
+    int num = SumNature(numBotton, iter, sum);
+    System.Console.WriteLine($"при M = {numBotton} и N = {numTop}  ->  {num}");
+}
+else
+    System.Console.WriteLine("Введеный  диапазон не корректен");
