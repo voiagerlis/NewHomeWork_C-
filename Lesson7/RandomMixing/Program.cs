a0eceae45a7cbf96d19109e0a952b2b773957aba
Vladimir <voiager@rambler.ru>
2023-06-08 10:23:14 +0300

    Решение задачи рандомной сортировки массива

--- Lesson7/RandomMixing/Program.cs
+++ Lesson7/RandomMixing/Program.cs
@@ -1,16 +1,8 @@
-﻿//
-
-void FillArray(int[,] array)
+﻿void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
             array[i, j] = new Random().Next(0, 99);
-}
-void FillArrayFlag(bool[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            array[i, j] = false;
 }
 void PrintArray(int[,] array)
 {
@@ -21,84 +13,17 @@ void PrintArray(int[,] array)
         System.Console.WriteLine();
     }
 }
-void PrnAr(int[] array)
+void Mixed(int[,] array)
 {
-    for (int i = 0; i < array.Length; i++)
-        System.Console.Write($"{array[i], 3}     ");
-    System.Console.WriteLine();
-}
-int[] Generator(bool[,] arFlag, int rows, int cols)
-{
-    bool FlagNum1 = false;
-    bool FlagNum2 = false;
-    int[] MemoIdx = new int[4];
-    while (FlagNum1 == false)
-    {
-        int x = new Random().Next(0, rows);
-        int y = new Random().Next(0, cols);
-        for (int i = 0; i < arFlag.GetLength(0); i++)
-            for (int j = 0; j < arFlag.GetLength(1); j++)
-            {
-                if (arFlag[x, y] == false)
-                {
-                    MemoIdx[0] = x;
-                    MemoIdx[1] = y;
-                    FlagNum1 = true;
-                    arFlag[x, y] = true;
-                }
-            }
-    }
-
-    while (FlagNum2 == false)
-    {
-        int x = new Random().Next(0, rows);
-        int y = new Random().Next(0, cols);
-        for (int i = 0; i < arFlag.GetLength(0); i++)
-            for (int j = 0; j < arFlag.GetLength(1); j++)
-            {
-                if (arFlag[x, y] == false)
-                {
-                    MemoIdx[2] = x;
-                    MemoIdx[3] = y;
-                    FlagNum2 = true;
-                    arFlag[x, y] = true;
-                }
-            }
-    }
-
-    return MemoIdx;
-}
-void PrintFlag(bool[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            System.Console.Write($"{array[i, j], 5}     ");
-        System.Console.WriteLine();
-    }
-}
-
-int[,] MixArray(int[,] array, bool[,] arFlag, int rows, int cols)
-{
-    int[] helper = new int[4];
-    int Proverka = (rows + 1) * (cols + 1);
-    int count = 0;
-    int tmp = 0;
-    for (int g = 0; g < rows * cols / 2; g++)
-    {
-       
-        helper = Generator(arFlag, rows, cols);
-        for (int i = 0; i < array.GetLength(0); i++)
-            for (int j = 0; j < array.GetLength(1); j++)
-            { 
-                tmp = array[helper[0], helper[1]];
-                Console.WriteLine($"*********  {tmp}");
-                array[helper[0], helper[1]] = array[helper[2], helper[3]];
-                array[helper[2], helper[3]] = tmp;
-            }
-    }
-
-    return array;
+    for (int i = array.GetLength(0) - 1; i >= 0; i--)
+        for (int j = array.GetLength(1) - 1; j >= 0; j--)
+        {
+            int n = new Random().Next(0, array.GetLength(0) - 1);
+            int m = new Random().Next(0, array.GetLength(1) - 1);
+            int tmp = array[n, m];
+            array[n, m] = array[i, j];
+            array[i, j] = tmp;
+        }
 }
 Console.Clear();
 Console.WriteLine("Введите количество строк");
@@ -107,19 +32,12 @@ Console.WriteLine("Введите количество столбцов");
 int cols = Convert.ToInt32(Console.ReadLine());
 if (rows * cols % 2 == 0)
 {
-    bool[,] arrayFlag = new bool[rows, cols];
     int[,] array = new int[rows, cols];
-    int[,] MixedArray = new int[rows, cols];
-    int[] helperArray = new int[4];
-    FillArrayFlag(arrayFlag);
     FillArray(array);
     PrintArray(array);
     Console.WriteLine("*****************************************");
-    MixedArray = MixArray(array, arrayFlag, rows - 1, cols - 1);
-    //PrintFlag(arrayFlag);
-    // PrnAr(helperArray);
-    // PrintFlag(arrayFlag);
-    PrintArray(MixedArray);
+    Mixed(array);
+    PrintArray(array);
 }
 else
     Console.WriteLine(
